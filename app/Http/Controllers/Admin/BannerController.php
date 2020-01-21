adfc6c0211fae368be279f35b8450f6ae76c486e
gaurav00012 <aggarwal.gaurav611@gmail.com>
2020-01-21 08:30:47 +0530

    banner done

--- app/Http/Controllers/Admin/BannerController.php
+++ app/Http/Controllers/Admin/BannerController.php
@@ -1,31 +1,83 @@
 <?php
 
-namespace App\Http\Controllers\API;
+namespace App\Http\Controllers\Admin;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\UploadFile;
 use App\Banner;
-
 use Carbon\Carbon;
+
 class BannerController extends Controller
 {
     public $successStatus = 200;
 
-    public function index(){
-        $getallBanners = Banner::select('banner_url')->get();
-        $bannerArray = array();
-        $url = array();
-        foreach($getallBanners as $key => $banner)
-        {
-           $url[$key] = asset('images/banner/'.$banner->banner_url);
-            //array_push($bannerArray,$url);
-        }
+    // public function index(){
+    //     $getallBanners = Banner::select('banner_url')->get();
+    //     $bannerArray = array();
+    //     $url = array();
+    //     foreach($getallBanners as $key => $banner)
+    //     {
+    //        $url[$key] = asset('images/banner/'.$banner->banner_url);
+    //         //array_push($bannerArray,$url);
+    //     }
         
-       $success['banners'] = json_encode($bannerArray);
-        return response()->json(['success'=>$url,$this->successStatus]);
-        //print_r (json_encode($bannerArray));
+    //    $success['banners'] = json_encode($bannerArray);
+    //     return response()->json(['success'=>$url,$this->successStatus]);
+    //     //print_r (json_encode($bannerArray));
+    // }
+
+    public function index()
+    {
+         return view("admin.banner.create");
     }
+
+    public function getallbanner()
+    {
+        return Banner::all();
+    }
+
+
+    public function store(Request $request)
+    {
+        //
+
+        $this->validate($request,[
+           
+            'bannerImage' => 'required',
+            // 'from_date' => 'required|date',
+            // 'to_date' => 'required|date',
+            // 'coupon_code' => 'required|unique:Promocodes',
+            // 'coupon_type' => 'required',
+            // 'coupon_value' => 'required',
+            // 'coupon_min_value' => 'required',
+            // 'coupon_max_value' => 'required',
+            // 'coupon_limit' => 'required',
+            // 'coupon_for' => 'required|in:opt,total,product',
+            // 'product_name' => 'required_if:coupon_for,==,product',
+            // 'product_link' => 'required_if:coupon_for,==,product',
+            // 'comments' => 'required',
+            //'created_by' => 'required'
+            ''
+           ]);
+    
+           
+            $couponImage = $request->file('bannerImage');
+            $imageExtension = $couponImage->getClientOriginalExtension();
+            $dir = 'assets/banners/';
+            $filename = 'banner'.'_'.time().'_'.date('Ymd').'.'.$imageExtension;
+
+            $banner = new Banner;
+            $banner->banner_url = $filename;
+            $banner->active_status = 1;
+            
+            $request->file('bannerImage')->move($dir, $filename);
+            $banner->save();
+
+            return 'banner_created';
+            // return response()->json(['success'=>$success],$this->successStatus);
+    }
+
     public function uploadBanner(Request $request){
         $this->validate($request,[
             'image' => 'required',
---
+++ resources/assets/js/admin/banner.js
@@ -0,0 +1,116 @@
+import swal from 'sweetalert';
+window.Vue = require('vue');
+import axios from 'axios';
+
+const app = new Vue({
+  el: '#banner',
+    data:{
+      CouponVal:{},
+      CouponValidationError:{},
+      allBanner : {},
+      editValidationError: {},
+      eCouponVal : {},
+      eCouponValidationError : {},
+    },
+    methods:{
+      addBannermodal()
+      {
+        $('#addbannermodal').modal('show');
+
+      },
+      addBanner()
+      {
+        
+         let bannerAddUrl = $('#urlBanneradd').val();
+         console.log(bannerAddUrl);
+         let _this = this;
+        
+         
+         let form_data = new FormData();
+        
+         form_data.append('bannerImage',$('#baner-image').prop('files')[0]);
+         // let form_data = new FormData();
+         console.log(form_data);
+       axios.post(bannerAddUrl,form_data)
+        .then(function(response){
+            console.log(response);
+          if(response.status == 200 && response.data == 'banner_created')
+          {
+              
+              $('#addbannermodal').modal('hide');
+             
+               _this.getallBanner();
+              swal("Banner Added Successfully!", "", "success");
+          }
+        }).
+        catch(function(error){
+           _this.shopkeeperValidationError = error.response.data.errors;
+          console.log(_this.shopkeeperValidationError);
+        });
+      },
+      getallBanner()
+        {
+          let _this = this;
+          let getAllBannerurl = $('#urlbannerget').val();
+          axios.get(getAllBannerurl)
+          .then(function(response){
+            _this.allBanner = response.data;
+            
+
+          }).
+          catch(function(error){
+
+          });
+    },
+       deleteCoupon(id)
+      {
+        let _this = this;
+        let deleteCouponUrl = $('#urlcoupondel').val();
+        swal({
+          title: "Are you sure?",
+          text: "Once deleted, you will not be able to recover this Coupon!",
+          icon: "warning",
+          buttons: true,
+          dangerMode: true,
+        })
+        .then((willDelete) => {
+          if (willDelete) {
+            axios.get(deleteCouponUrl+'/'+id)
+            .then(function(resp){
+              let respnc = resp.data;
+              if(respnc == 'coupondeleted')
+              {
+                //swal("Coupon deleted Successfully!", "", "success");
+                  _this.getallCoupon();
+                  swal("Coupon has been deleted!", {
+                    icon: "success",
+                  });
+              }
+            }).
+            catch(function(resp){
+              console.log(resp);
+            })
+
+          } else {
+            //swal("Your imaginary file is safe!");
+          }
+        });
+      }
+      
+      
+
+     
+      
+
+    },
+    mounted(){
+      this.getallBanner();
+
+    },
+    beforeDestroy()
+    {
+
+    }
+
+
+});
